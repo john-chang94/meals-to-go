6adf52b51f5a359c36f220833a1cbe86ab82e5ef
john-chang94 <j.chang94@yahoo.com>
2021-10-04 18:06:19 -0700

    init commit

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Canvas</Text>
     </View>
   );
 }
@@ -14,8 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
