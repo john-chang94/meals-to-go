b0c3d3a89652906cb715fd5ca441de7883e02203
john-chang94 <j.chang94@yahoo.com>
2021-10-05 19:40:14 -0700

    fixed android status bar dimensions

--- App.js
+++ App.js
@@ -1,18 +1,35 @@
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Canvas</Text>
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
+  search: {
+    backgroundColor: 'green',
+    padding: 16
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'yellow'
+  }
 });
