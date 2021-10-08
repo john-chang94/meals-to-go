cf65752715cc5c99ff0cd1c2ef4706563da2cfcc
john-chang94 <j.chang94@yahoo.com>
2021-10-07 20:38:07 -0700

    refactored component styles

--- src/components/spacer.js
+++ src/components/spacer.js
@@ -3,40 +3,40 @@ import styled, { useTheme } from "styled-components";
 
 // Referring to spacing index in theme
 const sizeVariant = {
-    small: 1,
-    medium: 2,
-    large: 3
-}
+  small: 1,
+  medium: 2,
+  large: 3,
+};
 
 const positionVariant = {
-    top: "margin-top",
-    right: "margin-right",
-    bottom: "margin-bottom",
-    left: "margin-left"
-}
+  top: "margin-top",
+  right: "margin-right",
+  bottom: "margin-bottom",
+  left: "margin-left",
+};
 
 const getVariant = (position, size, theme) => {
-    const sizeIndex = sizeVariant[size];
-    const property = positionVariant[position];
-    const value = theme.space[sizeIndex]
+  const sizeIndex = sizeVariant[size];
+  const property = positionVariant[position];
+  const value = theme.space[sizeIndex];
 
-    return `${property}: ${value}`; // Finally, build the css property needed
-}
+  return `${property}: ${value}`; // Finally, build the css property needed
+};
 
 const SpacerView = styled.View`
-${({ variant }) => variant};
+  ${({ variant }) => variant};
 `;
 
 // [ANDROID] Cannot directly return and render a Spacer component before app loads
 // so we use a normal component and render the SpacerView with any children components
 export default function Spacer({ position, size, children }) {
-    const theme = useTheme();
-    const variant = getVariant(position, size, theme);
+  const theme = useTheme();
+  const variant = getVariant(position, size, theme);
 
-    return <SpacerView variant={variant}>{children}</SpacerView>
+  return <SpacerView variant={variant}>{children}</SpacerView>;
 }
 
 Spacer.defaultProps = {
-    position: "top",
-    size: "small"
-}
+  position: "top",
+  size: "small",
+};
--- src/features/restaurants/components/restaurantInfoCard.js
+++ src/features/restaurants/components/restaurantInfoCard.js
@@ -1,58 +1,26 @@
 import React from "react";
-import styled from "styled-components";
-import { StyleSheet, Text, Image } from "react-native";
-import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
+
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 import Spacer from "../../../components/spacer";
+import { Text } from "../../../components/text";
 
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const Title = styled.Text`
-  color: ${(props) => props.theme.colors.ui.primary};
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-`;
-
-const Info = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Address = styled.Text`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-`;
-
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-end;
-`;
-
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]}
-  padding-bottom: ${(props) => props.theme.space[2]}
-`;
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+  Icon,
+  Address
+} from "./restaurantInfoCardStyles";
 
 export default function RestaurantInfoCard({ restaurant = {} }) {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     images = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
@@ -68,7 +36,7 @@ export default function RestaurantInfoCard({ restaurant = {} }) {
     <RestaurantCard>
       <RestaurantCardCover source={{ uri: images[0] }} />
       <Info>
-        <Title>{name}</Title>
+        <Text variant="label">{name}</Text>
         <Section>
           <Rating>
             {ratingArray.map(() => (
@@ -76,9 +44,14 @@ export default function RestaurantInfoCard({ restaurant = {} }) {
             ))}
           </Rating>
           <SectionEnd>
-            {isTemporarilyClosed && <Text>TEMPORARILY CLOSED</Text>}
-            <Spacer position="left" size="medium">
-            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            {isTemporarilyClosed && (
+              <Text variant="error">TEMPORARILY CLOSED</Text>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Icon source={{ uri: icon }} />
             </Spacer>
           </SectionEnd>
         </Section>
