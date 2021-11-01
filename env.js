736d386f7164ba389d7bb1fc0f87ed23f6ad1884
john-chang94 <j.chang94@yahoo.com>
2021-10-31 22:52:47 -0700

    update env.js

--- env.js
+++ env.js
@@ -2,8 +2,4 @@ const dev = "https://cf33-2603-8001-3140-784c-fc28-d118-35ee-9627.ngrok.io/meals
 const prod = "https://us-central1-meals-to-go-a18a6.cloudfunctions.net";
 
 export const isDevelopment = process.env.NODE_ENV === "development";
-<<<<<<< HEAD
 export const host = process.env.NODE_ENV === isDevelopment ? dev : prod;
-=======
-export const host = process.env.NODE_ENV === isDevelopment ? dev : prod;
->>>>>>> e2ad7880ea887dfea5e27674446deb38b29f911a
